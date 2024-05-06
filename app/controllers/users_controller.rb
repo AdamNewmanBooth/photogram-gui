0b9c9306e910e5b54603a9e23ebcfcbd8ca8cb0a
AdamNewmanBooth <164922100+AdamNewmanBooth@users.noreply.github.com>
2024-05-06 22:38:28 +0000

    commit2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,16 @@ class UsersController < ApplicationController
     @list_of_users = matching_users.order({ :username => :asc})
     render({ :template => "users_template/index"})
   end 
-end
+
+  def view
+    #Parameters: ["path)"]
+    url_username = params.fetch("path_username")
+    
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.at(0)
+
+
+        render({ :template => "users_template/view"})  
+    end 
+  end 
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
 
   #Users 
   get("/users", {:controller => "users", :action => "index"})
-  get("/users/:username", {:controller => "users", :action => "view"})
+  get("/users/:path_username", {:controller => "users", :action => "view"})
 
   #Photos
-
+  get("/photos", {:controller => "photos", :action => "index"})
+  get("/photos/:path_username", {:controller => "photos", :action => "view"})
 
 end
