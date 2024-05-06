ca927f74a0d81e2a10e54b5cdbce7656d79479a8
AdamNewmanBooth <164922100+AdamNewmanBooth@users.noreply.github.com>
2024-05-06 21:17:17 +0000

    commit1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
 
+
+  #Users 
+  get("/users", {:controller => "users", :action => "index"})
+  get("/users/:username", {:controller => "users", :action => "view"})
+
+  #Photos
+
+
 end
